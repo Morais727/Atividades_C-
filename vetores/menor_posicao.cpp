39d14ff8e51d42f4daf34ec44da8ac5fa8d0df8f
Marcos_Morais <moraismarcos727@gmail.com>
2024-11-07 19:06:37 -0300

    A

---
+++ vetores/menor_posicao.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vetor_5[5], vetor_organizado[5];
+
+    for (int i = 0; i<5; i++)
+    {
+        cin >> vetor_5[i];
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i; j < 5; j++)
+        {
+            if (vetor_5[i] < vetor_5[j])
+            {
+                vetor_organizado[i] = vetor_5[i];
+            }
+            else
+            {
+                vetor_organizado[i] = vetor_5[j];
+            }
+        }
+        
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << vetor_organizado[i] << " ";
+    }
+
+    return 0;
+}
